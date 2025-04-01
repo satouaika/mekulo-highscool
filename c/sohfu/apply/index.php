723f80a4b92e0ceb795f660eacc7f8880244ef45
satouaika <sato@nextreservation.jp>
2025-04-01 14:00:03 +0900

    20240401add
    応募前見学について

--- c/sohfu/apply/index.php
+++ c/sohfu/apply/index.php
@@ -70,20 +70,41 @@ $og_image = 'https://works.mekulo.jp/ogp-image.png';
 <div class="content_area">
 <main>
 <div class="fv_wr">
-	<img alt="" class="fv_image" src="../third.jpg">
+	<img alt="" class="fv_image" src="firstview2.jpg">
 	<div class="apply">
 		<h1 class="catch"><?= $co['company_name'] ?></h1>
 		<p class="tag">応募前見学ページ</p>
 	</div>
 </div>
 <section id="content" class="content">
-	<p class="catchcopy"><span>ダクト作り体験も！？</span><br>
-		<span>工場見学ツアーに参加しよう！</span></p>
-		<div class="content-wr">
-			<p>
-				サンプルテキストサンプルテキストサンプルサンプルサンプルサンプルテキストサンプルテキストサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルテキストサンプルテキストサンプルサンプルサンプルサンプルサンプルテキストサンプルテキストサンプルサンプルサンプルサンプルテキストサンプルテキストサンプルサンプルサンプルサンプル
-			</p>
-		</div>
+	<p class="catchcopy"><span>ダクト作りを覗いてみよう！</span></p>
+	<p class="cont">
+		創風の応募前見学では、一枚の金属板が機械によってさまざまな形に切り取られていく様子や、ダクトの形に組み立てられていく工程を間近で見学できます。<br>
+		ハンマーで叩くときの「カンカン」という音や、身体を使って金属板を曲げていく様子は創風ならでは。職場の雰囲気をチェックできるほか、先輩から直接仕事の話を聞けるかもしれません。<br>
+		また、ダクト作り体験も行います。実際に手を動かしてみることで、仕事のイメージが具体的になるでしょう。
+	</p>
+	<div class="opendate">
+		<p>開催日時</p>
+		<ul>
+			<li>2025年7月29日（火） 10時30分～、13時30分～</li>
+			<li>2025年7月31日（木） 10時30分～、13時30分～</li>
+			<li>2025年8月7日（木） 10時30分～、13時30分～</li>
+			<li>2025年8月21日（木） 10時30分～、13時30分～</li>
+		</ul>
+	</div>
+	<div class="flow">
+		<p>見学の流れ</p>
+		<ul>
+			<li>会社説明<span>10分程度</span></li>
+			<li>工場見学<span>20分程度</span></li>
+			<li>仕事体験<span>30分程度</span></li>
+		</ul>
+	</div>
+	<a href="../apply/#entry" class="entry-btn">
+		<img src="../../../flag.png" class="flag">
+		<span>応募前見学情報を見る</span>
+		<img src="../../../arrow.svg">
+	</a>
 </section>
 </main>
 <?php include $highschool_base . 'tmpl_navi.php' ?>
