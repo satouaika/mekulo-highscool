407f613099c0e6e53ec7e45bc2117c136bf84aec
satouaika <sato@nextreservation.jp>
2025-07-07 08:56:38 +0900

    kgk-sato
    初回コーディング、ロゴと写真入れただけです

---
+++ c/kgk/!data.php
@@ -0,0 +1,74 @@
+<?php
+$co = [
+    'slug' => 'miyama-seisakusho',
+    'image' => 'company.jpg',
+    'company_name' => 'ミヤマ製作所',
+    'catch_copy' => 'スゴ腕金属加工職人のあつまる職場',
+    'industry' => ['E'],
+    'post_code' => '386-1102',
+    'address' => '長野県上田市上田原１７８４',
+    'homepage' => 'https://miyamass.jp/',
+    'logo_filename' => 'logo.png',
+    'latitude' => 36.3855759193,//緯度
+    'longitude' => 138.2310452864,//経度
+    'human' => [
+        'image' => 'shomura.png',
+        'name' => '正村 欣生',
+    ]
+];
+
+// 募集職種
+$occupation = [
+    'job_kakou' => [
+        'name' => '金属加工職',
+        'url' => 'job_kakou',
+        'catch_copy' => '切粉の数だけ磨かれる職人技',
+        'work' => ['562'],
+        'fv_url' => 'first.jpg',
+        'latitude' => 36.3855759193,//緯度
+        'longitude' => 138.2310452864,//経度
+        'MBTI' =>  'IStJ',
+    ],
+];
+
+// 応募前見学
+$visit = [
+    [
+        'about' => '応募前見学',
+        'date' => [
+            [
+                'name' => '2025年8月4日（月） 9時00分～',
+                'date' => '2025-08-04',
+                'deadline' => '2025-08-03',//募集締切日
+            ],
+            [
+                'name' => '2025年8月5日（火） 9時00分～',
+                'date' => '2025-08-05',
+                'deadline' => '2025-08-04',//募集締切日
+            ],
+            [
+                'name' => '2025年8月6日（水） 9時00分～',
+                'date' => '2025-08-06',
+                'deadline' => '2025-08-05',//募集締切日
+            ],
+        ],
+        'catch_copy' => '',
+        'visit_img' =>'.jpg',
+        'content' => '見学希望の方は、お問い合わせください。',
+    ],
+];
+
+// 目次
+$toc = [
+    'top' => [
+        'name' => '企業TOP',
+        'icon' => 'company.png',
+        'url' => '',
+    ],
+    'job' => [
+        'name' => '募集職種',
+        'icon' => 'carrier.png',
+        'url' => '',
+        'submenu' => $occupation,
+		],
+	];
---
+++ c/kgk/job_kakou/index.php
@@ -0,0 +1,522 @@
+<?php
+$base = '../../../../';
+$highschool_base = '../../../';
+$company_base = '../';
+include_once $company_base . '!data.php';
+$current_url = $_SERVER['REQUEST_URI']; //現在のページのURLを表示
+$last_segment = basename($current_url);//現在のURLから末尾の職種URLの綴りを取得
+$catch_copy = $occupation[$last_segment]['catch_copy'] ?? '職種が見つかりません';//値があるか確認
+$name = $occupation[$last_segment]['name'] ?? '不明な職種';
+$meta_keywords = $occupation[$last_segment]['keyword'] ?? '';
+$meta_desc = $occupation[$last_segment]['description'] ?? '';
+
+
+// 現在のURLに一致しない職種を探す
+$next_job = null; // 初期値
+foreach ($occupation as $key => $job) {
+    if ($key !== $last_segment) {
+        $next_job = $job; // 最初に見つかった異なる職種をセット
+        break; // ループを終了
+    }
+}
+
+
+//========================================
+// ページのメタデータ
+$title = $name . ' - ' . $co['company_name'] . 'の新卒求人情報';
+$title_with_site = $title . ' | めくろうワークス';
+
+$keywords = $meta_keywords;
+$description = $meta_desc;
+
+$canonical_url = 'https://works.mekulo.jp/highschool/' . $co['slug'] . '/' . $last_segment . '/';
+
+$og_image = 'https://works.mekulo.jp/ogp-image.png';
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<?php include $base . '../tracking.php'; ?>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="<?= $highschool_base ?>highschool.css?v=2">
+<link rel="icon" href="<?= $base ?>mekulo-works-mark.png">
+<link rel="canonical" href="<?= $canonical_url ?>">
+<title><?= $title_with_site ?></title>
+<?php if ($description) { ?>
+<meta name="description" content="<?= $description ?>">
+<?php } ?>
+<?php if ($keywords) { ?>
+<meta name="keywords" content="<?= $keywords ?>">
+<?php } ?>
+<meta property="og:type" content="article">
+<meta property="og:title" content="<?= $title_with_site ?>">
+<?php if ($description) { ?>
+<meta property="og:description" content="<?= $description ?>">
+<?php } ?>
+<meta property="og:url" content="<?= $canonical_url ?>">
+<meta property="og:image" content="<?= $og_image ?>">
+<meta property="fb:app_id" content="534360210586966">
+<script type="application/ld+json">
+{
+	"@context": "https://schema.org",
+	"@type": "BreadcrumbList",
+	"itemListElement": [{
+		"@type": "ListItem",
+		"position": 1,
+		"name": "めくろうワークス",
+		"item": "https://works.mekulo.jp/"
+	},{
+		"@type": "ListItem",
+		"position": 2,
+		"name": "新卒求人特集",
+		"item": "https://works.mekulo.jp/highschool/"
+	},{
+		"@type": "ListItem",
+		"position": 3,
+		"name": "<?= $co['company_name'] ?>",
+		"item": "https://works.mekulo.jp/highschool/<?= $co['slug'] ?>/"
+	},{
+		"@type": "ListItem",
+		"position": 4,
+		"name": "<?= $name ?>",
+		"item": "https://works.mekulo.jp/highschool/<?= $co['slug'] ?>/<?= $last_segment ?>/"
+	}]
+}
+</script>
+</head>
+<body class="newgrads highschool">
+<?php include $highschool_base . 'tmpl_header.php'; ?>
+<div class="content_area">
+	<main>
+	<div class="fv_area">
+    <img alt="" class="fv_image" src="../first.jpg">
+    <hgroup>
+        <h1 class="catch"><?= $catch_copy ?></h1>
+        <p class="fv-tag"><?= $name ?></p>
+    </hgroup>
+    <?php if (count($occupation) >= 2): // 配列数が2以上か確認し、無い場合は非表示にする ?>
+        <a href="<?= $company_base . $next_job['url'] ?>/" class="btn">次の職種を見る</a>
+    <?php endif; ?>
+</div>
+<section class="page-nav">
+	<a href="#job_detail">
+		<div class="btn">
+			<img src="<?= $highschool_base ?>icon-1.png">
+			<p>仕事内容</p>
+		</div>
+		<div class="btn-af">
+			<span></span>
+		</div>
+	</a>
+	<a href="#employee_introduction">
+		<div class="btn">
+			<img src="<?= $highschool_base ?>icon-2.png">
+			<p>社員紹介</p>
+		</div>
+		<div class="btn-af">
+			<span></span>
+		</div>
+	</a>
+	<a href="#recruitment_requirements">
+		<div class="btn">
+			<img src="<?= $highschool_base ?>icon-3.png">
+			<p>募集要項</p>
+		</div>
+		<div class="btn-af">
+			<span></span>
+		</div>
+	</a>
+</section>
+<section class="g" id="job_detail">
+	<h2>仕事内容</h2>
+	<p>
+		ミヤマ製作所を端的にあらわすと金属加工職人の集まる会社です。<br>
+		どうやって金属を加工するかというと、NCフライス加工、マシニング加工、 旋盤加工っていう、ちょっとカッコいい名前の機械を使います。
+	</p>
+	<h3>NCフライス加工</h3>
+	<div class="images">
+		<img src="contents01.jpg" alt="" class="scene">
+		<img src="contents04.jpg" alt="" class="scene">
+	</div>
+	<p>
+		金属をガリガリ削って、立体的な形を作るための機械です。手動でも操作できるけど、プログラムを使って自動で削ることもできます。 
+	</p>
+	<h3>マシニング加工</h3>
+	<div class="images">
+		<img src="contents03.jpg" alt="" class="scene">
+	</div>
+	<p>
+		NCフライス加工よりもっと複雑な形を作れる機械 。プログラムを入力すると、自動で金属を削って穴を開けたり、形を整えたりすることができ、複雑な加工ができるのが特徴です。
+	</p>
+	<h3>旋盤加工</h3>
+	<div class="images">
+		<img src="contents02.jpg" alt="" class="scene">
+	</div>
+	<p>
+		金属をグルグル回して、円柱とか円錐みたいな形を作る機械です。材料を回転させて、そこに刃物（バイト）を当てて削っていきます。
+	</p>
+	<h3>仕事の魅力・特徴</h3>
+	<p>
+		未経験でもOK！ 先輩が一から丁寧に教えてくれるので、ゼロから技術を身につけられます。機械を使ったモノづくり！ 大きな金属を削ったり、精密な部品を作る仕事で、部品と部品がピタッと合ったときが、誇らしい仕事です。<br>
+		ミヤマ製作所の仕事は、ただ言われた作業をこなすだけではありません。完成品の先を想像し、自分の頭と手を使って考えながら加工していきます。ものづくりが好きで、コツコツ努力できる人にはピッタリの職場です。一方で、すぐに結果を求める人や、汚れる作業が苦手な人には、向いていないかもしれません。
+	</p>
+</section>
+<!--
+<section class="g">
+	<h2>写真で雰囲気を知る</h2>
+	<div class="NRGallery" data-width="640" data-height="480">
+		<div class="image_area">
+			<ul class="slides">
+				<li>
+					<img src="gallery1.jpg" alt="">
+				</li>
+				<li>
+					<img src="gallery2.jpg" alt="">
+				</li>
+				<li>
+					<img src="gallery3.jpg" alt="">
+				</li>
+				<li>
+					<img src="gallery4.jpg" alt="">
+				</li>
+			</ul>
+			<div class="button left_btn">&lt;</div>
+			<div class="button right_btn">&gt;</div>
+		</div>
+	</div>
+</section>
+-->
+<section class="g" id="employee_introduction">
+	<h2>社員紹介</h2>
+	<ul class="Lightbox">
+		<li class="thumbnails">
+			<div class="thumbnail">
+				<img src="shain.jpg" alt="">
+				<hgroup>
+					<h3>Sさん</h3>
+					<p>
+						「仕事に前向きな人ばかりの環境！」<br>
+						前職は手を抜くのが当たり前の職場でした。<br>
+						それが嫌でミヤマ製作所に転職したのですが、ここではみんな真剣に仕事に向き合い、品質の高いものづくりをしています。安心して長く働ける環境です。
+					</p>
+				</hgroup>
+			</div>
+			<p>Sさん</p>
+		</li>
+	</ul>
+</section>
+<section class="g requirements" id="recruitment_requirements">
+	<h2>募集要項</h2>
+	<div class="tabs">
+		<button class="tab1">募集要項</button>
+		<button class="tab2">青少年雇用情報</button>
+		<button class="tab3">選考方法</button>
+	</div>
+	<div class="requirement">
+		<div class="content recruitment">
+			<h3>仕事内容</h3>
+			<dl>
+				<div>
+					<dt>募集職種</dt>
+					<dd>金属加工及び検査</dd>
+				</div>
+				<div>
+					<dt>雇用形態</dt>
+					<dd>正社員</dd>
+				</div>
+				<div>
+					<dt>雇用期間</dt>
+					<dd>定めなし</dd>
+				</div>
+				<div>
+					<dt>契約更新の可能性</dt>
+					<dd>なし</dd>
+				</div>
+				<div>
+					<dt>試用期間</dt>
+					<dd>あり</dd>
+				</div>
+				<div>
+					<dt>試用期間中の労働条件</dt>
+					<dd>同条件</dd>
+				</div>
+				<div>
+					<dt>就業場所</dt>
+					<dd>
+						〒386-1102<br>
+						長野県上田市上田原１７８４
+					</dd>
+				</div>
+				<div>
+					<dt>アクセス</dt>
+					<dd>別所線 赤坂上駅から徒歩10分</dd>
+				</div>
+				<div>
+					<dt>受動喫煙対策</dt>
+					<dd>あり</dd>
+				</div>
+				<div>
+					<dt>受動喫煙対策に関する特記事項</dt>
+					<dd>屋内禁煙</dd>
+				</div>
+				<div>
+					<dt>マイカー通勤</dt>
+					<dd>可</dd>
+				</div>
+				<div>
+					<dt>転勤の可能性</dt>
+					<dd>なし</dd>
+				</div>
+				<div>
+					<dt>必要な知識・技能等</dt>
+					<dd>不問</dd>
+				</div>
+			</dl>
+			<h3>賃金・手当</h3>
+			<dl>
+				<div>
+					<dt>賃金形態</dt>
+					<dd>月給</dd>
+				</div>
+				<div>
+					<dt>月平均労働日数</dt>
+					<dd>21.6日</dd>
+				</div>
+				<div>
+					<dt>毎月の賃金</dt>
+					<dd>現行</dd>
+				</div>
+				<div>
+					<dt>基本給</dt>
+					<dd>180,000円</dd>
+				</div>
+				<div>
+					<dt>定期的に支払われる手当</dt>
+					<dd>-</dd>
+				</div>
+				<div>
+					<dt>固定残業代</dt>
+					<dd>なし</dd>
+				</div>
+				<div>
+					<dt>初任給</dt>
+					<dd>180,000円</dd>
+				</div>
+				<div>
+					<dt>特別に支払われる手当①</dt>
+					<dd>技術手当（3,000円）</dd>
+				</div>
+				<div>
+					<dt>特別に支払われる手当②</dt>
+					<dd>皆勤手当（3,000円）</dd>
+				</div>
+				<div>
+					<dt>賃金締切日</dt>
+					<dd>毎月20日</dd>
+				</div>
+				<div>
+					<dt>通勤手当</dt>
+					<dd>あり（上限12,000円）</dd>
+				</div>
+				<div>
+					<dt>昇給</dt>
+					<dd>あり</dd>
+				</div>
+				<div>
+					<dt>賞与</dt>
+					<dd>あり（1.3か月分）</dd>
+				</div>
+			</dl>
+			<h3>労働時間</h3>
+			<dl>
+				<div>
+					<dt>就業期間</dt>
+					<dd>8時30分〜17時35分</dd>
+				</div>
+				<div>
+					<dt>時間外労働</dt>
+					<dd>月平均20時間</dd>
+				</div>
+				<div>
+					<dt>休日</dt>
+					<dd>週休2日制（土日・祝日・夏季休暇）</dd>
+				</div>
+				<div>
+					<dt>その他休日</dt>
+					<dd>年末年始、お盆、など</dd>
+				</div>
+			</dl>
+			<h3>保険・年金・定年等</h3>
+			<dl>
+				<div>
+					<dt>加入保険等</dt>
+					<dd>厚生年金保険、健康保険、雇用保険、労災保険</dd>
+				</div>
+				<div>
+					<dt>企業年金</dt>
+					<dd>-</dd>
+				</div>
+				<div>
+					<dt>退職金制度</dt>
+					<dd>あり（勤続年数3年以上）</dd>
+				</div>
+				<div>
+					<dt>定年制</dt>
+					<dd>あり</dd>
+				</div>
+				<div>
+					<dt>一律定年制</dt>
+					<dd>65歳</dd>
+				</div>
+				<div>
+					<dt>再雇用制度</dt>
+					<dd>なし</dd>
+				</div>
+				<div>
+					<dt>勤務延長</dt>
+					<dd>なし</dd>
+				</div>
+				<div>
+					<dt>入居可能住宅</dt>
+					<dd>なし</dd>
+				</div>
+			</dl>
+		</div>
+		<div class="content career">
+			<h3>青少年雇用情報</h3>
+			<dl>
+				<div>
+					<dt>直近3年間の新卒者等採用者数</dt>
+					<dd>前年度:0人、2年度前：0人、3年度前：0人</dd>
+				</div>
+				<div>
+					<dt>直近3年間の新卒者等離職者数</dt>
+					<dd>前年度:0人、2年度前：0人、3年度前：0人</dd>
+				</div>
+				<div>
+					<dt>男性の新卒者等採用数</dt>
+					<dd>前年度:0人、2年度前：0人、3年度前：0人</dd>
+				</div>
+				<div>
+					<dt>女性の新卒者等採用数</dt>
+					<dd>前年度:0人、2年度前：0人、3年度前：0人</dd>
+				</div>
+				<div>
+					<dt>平均継続勤務年数</dt>
+					<dd>
+						10年
+					</dd>
+				</div>
+				<div>
+					<dt>従業員の平均年齢</dt>
+					<dd>42.7歳</dd>
+				</div>
+				<div>
+					<dt>研修の有無</dt>
+					<dd>あり</dd>
+				</div>
+				<div>
+					<dt>研修内容</dt>
+					<dd>
+						機械操作など
+					</dd>
+				</div>
+				<div>
+					<dt>自己啓発支援の有無</dt>
+					<dd>なし</dd>
+				</div>
+				<div>
+					<dt>メンター制度の有無</dt>
+					<dd>なし</dd>
+				</div>
+				<div>
+					<dt>キャリアコンサルティング制度の有無</dt>
+					<dd>なし</dd>
+				</div>
+				<div>
+					<dt>社内検定等の有無</dt>
+					<dd>なし</dd>
+				</div>
+				<div>
+					<dt>前年度の月平均所定労働時間</dt>
+					<dd>20時間</dd>
+				</div>
+				<div>
+					<dt>前事業年度の有給休暇の平均取得日数</dt>
+					<dd>16日</dd>
+				</div>
+				<div>
+					<dt>前事業年度の育児休業取得者数</dt>
+					<dd>女性：1人、男性：0人</dd>
+				</div>
+				<div>
+					<dt>前事業年度の出産者数</dt>
+					<dd>1名</dd>
+				</div>
+				<div>
+					<dt>女性役員割合</dt>
+					<dd>20%</dd>
+				</div>
+				<div>
+					<dt>女性の管理職割合</dt>
+					<dd>10%</dd>
+				</div>
+				<div>
+					<dt>区分の名称</dt>
+					<dd>管工事業</dd>
+				</div>
+			</dl>
+		</div>
+		<div class="content selection">
+			<h3>選考方法</h3>
+			<dl>
+				<div>
+					<dt>求人数</dt>
+					<dd>1名</dd>
+				</div>
+				<div>
+					<dt>応募前職場見学</dt>
+					<dd>
+						2025年8月4日（月） 9時00分～<br>
+						2025年8月5日（火） 9時00分～<br>
+						2025年8月6日（水） 9時00分～
+					</dd>
+				</div>
+				<div>
+					<dt>選考方法</dt>
+					<dd>面接、適性検査</dd>
+				</div>
+				<div>
+					<dt>選考旅費</dt>
+					<dd>なし</dd>
+				</div>
+				<div>
+					<dt>選考結果通知</dt>
+					<dd>面接後10日以内</dd>
+				</div>
+				<div>
+					<dt>選考担当者</dt>
+					<dd>
+						山田しのぶ
+					</dd>
+				</div>
+			</dl>
+		</div>
+	</div>
+	<div>
+		<a href="../apply/#entry" class="btn_entry">
+			<img src="../../../flag.png" class="flag">
+			<span>応募前見学情報を見る</span>
+			<img src="../../../arrow.svg">
+		</a>
+	</div>
+</section>					
+</main>
+<?php include $highschool_base . 'tmpl_navi.php' ?>
+</div>
+<?php include $base . '../footer.php' ?>
+<script src="<?= $highschool_base ?>animation.js"></script>
+<script src="<?= $highschool_base ?>lightbox.js"></script>
+<script src="<?= $highschool_base ?>NRGallery.js"></script>
+</body>
