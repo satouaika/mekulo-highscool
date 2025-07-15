5c4243921500d533d27e985ad55721e03dde0646
satouaika <sato@nextreservation.jp>
2025-07-15 15:14:14 +0900

    furea-sato0715
    求人票反映

--- c/furea-original/apply/index.php
+++ c/furea-original/apply/index.php
@@ -81,10 +81,11 @@ $og_image = 'https://works.mekulo.jp/ogp-image.png';
 	<div class="opendate">
 		<p>開催日時</p>
 		<ul>
-			<li>2025年 月  日（ ）   時  分～</li>
-			<li>2025年 月  日（ ）   時  分～</li>
+			随時開催中！<br>
+			お気軽にお問い合わせください。
 		</ul>
 	</div>
+	<!--
 	<div class="flow">
 		<p>見学の流れ</p>
 		<ul>
@@ -92,6 +93,7 @@ $og_image = 'https://works.mekulo.jp/ogp-image.png';
 			<li>工場見学<span>  分程度</span></li>
 		</ul>
 	</div>
+	-->
 	<p class="entry_txt">応募前見学をご希望の方は、担任の先生や、進路指導の先生にご相談ください。</p>
 </section>
 </main>
